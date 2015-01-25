7abdb29e5dbf3ebb297f2a00f69757111db372bf
John Drago <john@Johns-MacBook-Pro.local>
2015-01-24 16:14:39 -0800

    No more rbenv -- things look good

--- Berksfile
+++ Berksfile
@@ -3,7 +3,5 @@ site :opscode
 
 cookbook 'apt'
 cookbook 'build-essential'
-cookbook 'ruby_build'
-cookbook 'rbenv', git: 'https://github.com/muskox/chef-rbenv'
 
 metadata
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,51 +7,18 @@
 file '/tmp/hello.txt' do
   content "hello world -- #{node['test-cookbook']['foo']}\n"
 end
-ruby_version = '2.0.0-p576'
-node.default['rbenv']['user_installs'] = [
-  user: 'vagrant',
-  rubies: [ruby_version],
-  global: ruby_version,
-  gems: {
-    ruby_version: [
-      {name: 'bundler'}
-    ]
-  }
-]
 
+include_recipe  'apt'
+package  'build-essential'
+package 'ruby2.0'
+package 'ruby2.0-dev'
 
-include_recipe  "apt"
-include_recipe  "build-essential"
-include_recipe  "ruby_build"
-include_recipe 'rbenv::user'
-
-
-
-#package 'libxml2-dev'
-#package 'libxslt-dev'
-
-# ruby_version = '2.0.0-p576'
-
-# rbenv_ruby ruby_version
-# rbenv_global ruby_version
-
-__END__
-
-bash 'install rbenv ruby, dammit' do
-  not_if "rbenv versions | grep '#{ruby_version}'"
-  code "rbenv install #{ruby_version} && rbenv global #{ruby_version} && rbenv rehash"
+bash 'make ruby2.0 the default' do
+  not_if 'ruby --version | grep "ruby 2."'
+  code 'ln -sf /usr/bin/ruby2.0 /usr/bin/ruby && ln -sf /usr/bin/gem2.0 /usr/bin/gem'
 end
 
-bash 'install bundler' do
-  not_if 'bundle --version'
+bash 'install bundler gem' do
+  not_if 'bundler --version'
   code 'gem install bundler'
-end
-
-bash 'chown rbenv shims' do
-  code 'chown vagrant:vagrant -R /home/vagrant/.rbenv'
-end
-
-bash 'rbenv rehash' do
-  user 'vagrant'
-  code 'rbenv rehash'
 end
